747a5519863cb2c9f2d052eca346a4c67d9f798e
jtaseff <jtaseff@gmail.com>
2015-04-01 14:43:46 -0500

    empty project hw1

---
+++ HW1/hw1.X/hw1main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   hw1main.c
+ * Author: John
+ *
+ * Created on April 1, 2015, 2:17 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
