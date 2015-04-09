c0d1f08e5d834ca9348945fbe4d4d66d2c503808
jtaseff <jtaseff@gmail.com>
2015-04-09 15:41:01 -0500

    board cam finished

--- PIC programming/HW1/hw1.X/nbproject/Makefile-default.mk
+++ PIC programming/HW1/hw1.X/nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=hw1main.c newfile.c
+SOURCEFILES_QUOTED_IF_SPACED=hw1main.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/hw1main.o ${OBJECTDIR}/newfile.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/hw1main.o.d ${OBJECTDIR}/newfile.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/hw1main.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/hw1main.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/hw1main.o ${OBJECTDIR}/newfile.o
+OBJECTFILES=${OBJECTDIR}/hw1main.o
 
 # Source Files
-SOURCEFILES=hw1main.c newfile.c
+SOURCEFILES=hw1main.c
 
 
 CFLAGS=
@@ -100,24 +100,12 @@ ${OBJECTDIR}/hw1main.o: hw1main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${RM} ${OBJECTDIR}/hw1main.o 
 	@${FIXDEPS} "${OBJECTDIR}/hw1main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/hw1main.o.d" -o ${OBJECTDIR}/hw1main.o hw1main.c   
 	
-${OBJECTDIR}/newfile.o: newfile.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/newfile.o.d 
-	@${RM} ${OBJECTDIR}/newfile.o 
-	@${FIXDEPS} "${OBJECTDIR}/newfile.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/newfile.o.d" -o ${OBJECTDIR}/newfile.o newfile.c   
-	
 else
 ${OBJECTDIR}/hw1main.o: hw1main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/hw1main.o.d 
 	@${RM} ${OBJECTDIR}/hw1main.o 
 	@${FIXDEPS} "${OBJECTDIR}/hw1main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/hw1main.o.d" -o ${OBJECTDIR}/hw1main.o hw1main.c   
-	
-${OBJECTDIR}/newfile.o: newfile.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/newfile.o.d 
-	@${RM} ${OBJECTDIR}/newfile.o 
-	@${FIXDEPS} "${OBJECTDIR}/newfile.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/newfile.o.d" -o ${OBJECTDIR}/newfile.o newfile.c   
 	
 endif
 
