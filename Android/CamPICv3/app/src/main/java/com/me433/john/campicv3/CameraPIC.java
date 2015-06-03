30b10d99c7109f666a05f7c2161ccd139d458c1e
jtaseff <jtaseff@gmail.com>
2015-06-03 11:33:39 -0500

    trying to detect being lost/camera washing out

--- Android/CamPICv3/app/src/main/java/com/me433/john/campicv3/CameraPIC.java
+++ Android/CamPICv3/app/src/main/java/com/me433/john/campicv3/CameraPIC.java
@@ -2,6 +2,7 @@ package com.me433.john.campicv3;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Camera;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
@@ -9,6 +10,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.hoho.android.usbserial.driver.CdcAcmSerialDriver;
@@ -34,8 +37,17 @@ public class CameraPIC extends Activity implements Camera.PreviewCallback {
 
     private TextView textStatusCam;
     private TextView textStatusComm;
-    private TextView textStatusValue;
+    private TextView textStatusPWM;
     private TextView textLine1Value;
+
+    private SeekBar seekPower;
+    private SeekBar seekProp;
+    private SeekBar seekThresh;
+    private SeekBar seekLinepos;
+    private TextView seekPowerText;
+    private TextView seekPropText;
+    private TextView seekThreshText;
+    private TextView seekLineposText;
 
     UsbManager manager = null;
     UsbSerialDriver driver = null;
@@ -46,6 +58,12 @@ public class CameraPIC extends Activity implements Camera.PreviewCallback {
 
     int numFrames = 0;
 
+    double slowing = 0.5;   //  overall max power factor
+    double Pscale = 0.5;    //  scaling factor - proportional control
+    int threshold = 100;    //  image analysis dark pixel threshold
+    int linePos = 200;     //  position of line to look at
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,22 +71,100 @@ public class CameraPIC extends Activity implements Camera.PreviewCallback {
 
         textStatusCam = (TextView) findViewById(R.id.textStatusCam);
         textStatusComm = (TextView) findViewById(R.id.textStatusComm);
-        textStatusValue = (TextView) findViewById(R.id.textStatusValue);
         textLine1Value = (TextView) findViewById(R.id.textLine1Value);
+        textStatusPWM = (TextView) findViewById(R.id.textStatusPWM);
 
+        seekPower = (SeekBar) findViewById(R.id.seekPower);
+        seekProp = (SeekBar) findViewById(R.id.seekProp);
+        seekPowerText = (TextView) findViewById(R.id.seekPowerText);
+        seekPropText = (TextView) findViewById(R.id.seekPropText);
+        seekThresh = (SeekBar) findViewById(R.id.seekThresh);
+        seekThreshText = (TextView) findViewById(R.id.seekThreshText);
+        seekLinepos = (SeekBar) findViewById(R.id.seekLinepos);
+        seekLineposText = (TextView) findViewById(R.id.seekLineposText);
+
+        seekPower.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                slowing = (double) seekBar.getProgress() / 100;
+                seekPowerText.setText(seekBar.getProgress() + "% Power");
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        seekProp.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Pscale = (double) seekBar.getProgress() / 100;
+                seekPropText.setText(seekBar.getProgress() + "% Proportional");
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
+
+        seekThresh.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                threshold = seekBar.getProgress();
+                seekThreshText.setText(seekBar.getProgress() + " img threshold");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+
+        });
+
+
+
+        seekLinepos.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                linePos = seekBar.getProgress();
+                seekLineposText.setText(seekBar.getProgress() + " line position");
+
+                ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) textLine1Value.getLayoutParams();
+                params.bottomMargin = (1000 - linePos)*3/8 + 80;
+                textLine1Value.setLayoutParams(params);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
 
         initCameraPreview();
-        camera.stopPreview();
-        textStatusCam.setText("video feed ready, paused");
+        textStatusCam.setText("video feed running");
+
 
 
     }
 
     //Buttons
+    public void btnFocus(View view) {
+        if (camera != null) {
+            camera.autoFocus(null);
+        }
+    }
+
     public void btnCamStart(View view) {
         if (camera != null) {
             camera.startPreview();
             textStatusCam.setText("Video feed started");
+
+            textStatusCam.setTextColor(Color.BLACK);
         }
     }
 
@@ -118,12 +214,14 @@ public class CameraPIC extends Activity implements Camera.PreviewCallback {
 
         textStatusComm.setText("data link opened");
 
-        byte[] outmsg = {'f', 'a', 'r', 't'};
+        byte[] outmsg = {0, 0, 0, 0};
         try {
             port.write(outmsg, 500);
         } catch (IOException e) {
             Log.e(st, "error writing to port: " + e.getMessage());
         }
+
+        serialReady = true;
 
 
     }
@@ -140,21 +238,101 @@ public class CameraPIC extends Activity implements Camera.PreviewCallback {
 
     }
 
+    String PWMstatus = "Stopped  ";
+
     public void btnMotionStart(View view) {
-
-
-        textStatusValue.setText("sent a thing");
-
+        motionOn = 2;
+        updateRobot();
+        PWMstatus = "Started  ";
 
     }
 
-    public void btnMotionStop(View view) {
+    public void btnMotionIdle(View view) {
+        motionOn = 1;
+        updateRobot();
+        PWMstatus = "Idle  ";
+    }
 
-        textStatusValue.setText("stopped. Last value NULL");
+    public void btnMotionStop(View view) {
+        motionOn = 0;
+        updateRobot();
+        PWMstatus = "Stopped  ";
     }
 
 
     //Serial stuff below
+    boolean serialReady = false;
+    byte motionOn = 0;
+    int left;
+    int right;
+    int diff;
+    boolean lastwasleft;
+    boolean lost;
+
+    byte[] outmsg = new byte[4];
+
+    private void updateRobot() {
+        if (motionOn < 2) {
+            // idle or stopped
+            left = 0;
+            right = 0;
+        }
+        if (motionOn == 2) {
+            // motion enabled
+
+
+            if (mean < 360){
+                lost = false;
+                lastwasleft = false;
+                diff = 360 - mean;
+                right = (int) ((255 - Pscale*diff) * slowing);
+                left = (int) (255 * slowing);
+            } else if (mean > 360) {
+                lost = false;
+                lastwasleft = true;
+                diff = mean - 360;
+                right = (int) (255 * slowing);
+                left = (int) ((255 - Pscale*diff) * slowing);
+            } else {    // we have 360 - maybe keep the last values??
+                if (!lost) {
+                    textStatusCam.setText("LOST");
+                    textStatusCam.setTextColor(Color.RED);
+                    lost = true;
+                    left = left / 2;
+                    right = right / 2;
+                }
+//                if (lastwasleft) {
+//                    left = 0;
+//                    right = (int) (255 * slowing * 0.5);
+//                }
+//                else {
+//                    left = (int) (255 * slowing * 0.5);
+//                    right = 0;
+//                }
+            }
+            if (left < 0)
+                left = 0;
+            if (left > 255)
+                left = 255;
+            if (right < 0)
+                right = 0;
+            if (right > 255)
+                right = 255;
+
+        }
+        outmsg[0] = motionOn;
+        outmsg[1] = (byte) left;
+        outmsg[2] = (byte) right;
+        textStatusPWM.setText(PWMstatus + outmsg[0] + " : " + left + " : " + right);
+
+        if (serialReady) {
+            try {
+                port.write(outmsg, 500);
+            } catch (IOException e) {
+                Log.e(st, "error writing to port: " + e.getMessage());
+            }
+        }
+    }
 
 
     //Camera stuff below
@@ -191,8 +369,9 @@ public class CameraPIC extends Activity implements Camera.PreviewCallback {
     int value;
     int findSum;
     int findNum;
-    int linePos = 1000;
     int mean;
+
+
 
     @Override
     public void onPreviewFrame(byte[] data, Camera camera) {
@@ -217,7 +396,7 @@ public class CameraPIC extends Activity implements Camera.PreviewCallback {
             //go across the row and tally up the location of all dark pixels
             for (int ii = 0; ii < previewSize.height; ii++) {
                 value = grpx[(ii * previewSize.width) + linePos + jj];
-                if (value < 100) {
+                if (value < threshold) {
                     findSum += ii;
                     findNum++;
                 }
@@ -231,40 +410,17 @@ public class CameraPIC extends Activity implements Camera.PreviewCallback {
 
         textLine1Value.setText("" + mean);
 
+        updateRobot();
+
     }
 
-
-//    @Override
-//    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-//        // Now that the size is known, set up the camera parameters and begin
-//        // the preview.
-//        parameters = camera.getParameters();
-//
-//        // use the default preview size supported by this device
-//        List<Camera.Size> previewSizes = parameters.getSupportedPreviewSizes();
-//        previewSize = previewSizes.get(1);
-//        parameters.setPreviewSize(previewSize.width, previewSize.height);
-//
-//        // want a high-quality, small image
-//        parameters.setJpegQuality(95);
-//        parameters.setPictureSize(640, 480);
-//        parameters.setColorEffect(Camera.Parameters.EFFECT_MONO); // try EFFECT_BLACKBOARD, EFFECT_NEGATIVE, EFFECT_WHITEBOARD
-//
-//        camera.setParameters(parameters);
-//        camera.setDisplayOrientation(90); // correct preview rotation
-//        camera.startPreview();
-//        camera.setPreviewCallback(this);
-//    }
-
-
-//    @Override
-//    public void surfaceCreated(SurfaceHolder holder) {
-//        //super.surfaceCreated(holder);
-//    }
 
 
     @Override
     protected void onPause() {
+
+        motionOn = 0;
+        updateRobot();
 
         if (camera != null) {
             camera.stopPreview();
@@ -284,6 +440,10 @@ public class CameraPIC extends Activity implements Camera.PreviewCallback {
             camera.release();
             camera = null;
         }
+
+        motionOn = 0;
+        updateRobot();
+
         if (port != null) {
             try {
                 port.close();
